1b72b1d54ee1527bf05bec9769d15110428d75e7
TianCal <carlostian927@berkeley.edu>
2020-07-09 13:01:43 +0800

    Pairing Enabled(With bugs)

--- app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
+++ app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
@@ -1,20 +1,33 @@
 package com.citrix.taskshiftonandroid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.companion.AssociationRequest;
+import android.companion.BluetoothDeviceFilter;
+import android.companion.CompanionDeviceManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.os.Build;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 1;
+    //private static final int REQUEST_ENABLE_BT = 1;
+    private CompanionDeviceManager deviceManager;
+    private AssociationRequest pairingRequest;
+    private BluetoothDeviceFilter deviceFilter;
 
+    private static final int SELECT_DEVICE_REQUEST_CODE = 42;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +46,45 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);*/
         }
+        deviceManager = getSystemService(CompanionDeviceManager.class);
+        deviceFilter = new BluetoothDeviceFilter.Builder().build();
+        pairingRequest = new AssociationRequest.Builder()
+                .addDeviceFilter(deviceFilter)
+                .setSingleDevice(false)
+                .build();
 
+        deviceManager.associate(pairingRequest,
+                new CompanionDeviceManager.Callback() {
+                    @Override
+                    public void onDeviceFound(IntentSender chooserLauncher) {
+                        try {
+                            startIntentSenderForResult(chooserLauncher,
+                                    SELECT_DEVICE_REQUEST_CODE, null, 0, 0, 0);
+                        } catch (IntentSender.SendIntentException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(CharSequence charSequence) {
+
+                    }
+                },
+                null);
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_DEVICE_REQUEST_CODE &&
+                resultCode == Activity.RESULT_OK) {
+            // User has chosen to pair with the Bluetooth device.
+            BluetoothDevice deviceToPair =
+                    data.getParcelableExtra(CompanionDeviceManager.EXTRA_DEVICE);
+            deviceToPair.createBond();
 
+            // ... Continue interacting with the paired device.
+            setContentView(R.layout.no_bluetooth);
+        }
+    }
 }
