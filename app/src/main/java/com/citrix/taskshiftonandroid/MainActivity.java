1fa9d7bd301b531272543d38256f9e1862cbb8b8
TianCal <carlostian927@berkeley.edu>
2020-07-09 11:52:15 +0800

    Enable discoverability.

--- app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
+++ app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
@@ -3,8 +3,13 @@ package com.citrix.taskshiftonandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Check for available Bluetooth adapter and enables it if it isn't.
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             setContentView(R.layout.no_bluetooth);
-        } else if (!bluetoothAdapter.isEnabled()) {
+        } else {
+            // Enable  bluetooth and discoverability at the same time
+            Intent discoverableIntent =
+                    new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+            /* if (!bluetoothAdapter.isEnabled())
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);*/
         }
-    }
 
+    }
 
 }
