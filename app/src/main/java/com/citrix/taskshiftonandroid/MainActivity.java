2ad9363a2ffaba33e2bbcb21e3ef5d804cc10039
TianCal <carlostian927@berkeley.edu>
2020-07-13 21:22:52 +0800

    Update MainActivity.java

--- app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
+++ app/src/main/java/com/citrix/taskshiftonandroid/MainActivity.java
@@ -3,9 +3,11 @@ package com.citrix.taskshiftonandroid;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.companion.AssociationRequest;
 import android.companion.BluetoothDeviceFilter;
@@ -17,16 +19,27 @@ import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.ParcelUuid;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //客户端服务端一体
+    private BluetoothSocket clientSocket;
+    private BluetoothDevice deviceToPair;
+    private OutputStream os;
+    private AcceptThread ac;
 
     //private static final int REQUEST_ENABLE_BT = 1;
     private CompanionDeviceManager deviceManager;
@@ -34,7 +47,9 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothDeviceFilter deviceFilter;
     //private UUID MY_UUID = UUID.fromString("try this");
     private static final int SELECT_DEVICE_REQUEST_CODE = 42;
-    private BluetoothDevice deviceToPair;
+
+    private BluetoothAdapter mBlueAdapter;
+    //配对成功的Receiver
     DynamicReceiver dynamicReceiver = new DynamicReceiver();
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -42,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Check for available Bluetooth adapter and enables it if it isn't.
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
+        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBlueAdapter == null) {
             setContentView(R.layout.no_bluetooth);
-        } else if (bluetoothAdapter.getScanMode() !=
+        } else if (mBlueAdapter.getScanMode() !=
                 BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
             // Enable  bluetooth and discoverability at the same time
             Intent discoverableIntent =
@@ -85,7 +100,9 @@ public class MainActivity extends AppCompatActivity {
                         null);
             }
         });
-
+        //开启接受线程
+        ac = new AcceptThread();
+        ac.start();
     }
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -116,17 +133,83 @@ public class MainActivity extends AppCompatActivity {
             int bonded = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.BOND_NONE);
             if (bonded == BluetoothDevice.BOND_BONDED) {
                 Toast.makeText(context,"配对成功,正在连接: " + deviceToPair.getName(), Toast.LENGTH_SHORT).show();
-                BluetoothSocket socket;
-                try {
-                    socket = deviceToPair.createRfcommSocketToServiceRecord(UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d"));
-                    socket.connect();
-                    Toast.makeText(context,"已与 " + deviceToPair.getName() + "连接。", Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    Toast.makeText(context,"未与 " + deviceToPair.getName() + "连接。", Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                }
+                tryConnect();
+                Toast.makeText(context,"已与 " + deviceToPair.getName() + "连接。", Toast.LENGTH_SHORT).show();
             } else if (bonded == BluetoothDevice.BOND_NONE) {
                 Toast.makeText(context,"配对失败,请重试", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void tryConnect() {
+        // 主动连接蓝牙
+        try {
+            // 判断是否在搜索,如果在搜索，就取消搜索
+            if (mBlueAdapter.isDiscovering()) {
+                mBlueAdapter.cancelDiscovery();
+            }
+            try {
+
+                clientSocket = deviceToPair
+                        .createRfcommSocketToServiceRecord(UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d"));
+                clientSocket.connect();
+                os = clientSocket.getOutputStream();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext()," " + deviceToPair.getName() + "连接失败。", Toast.LENGTH_SHORT).show();
+            }
+            if (os != null) {
+                os.write("老板，准备接受任务".getBytes());
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            Toast.makeText(getApplicationContext(), String.valueOf(msg.obj),
+                    Toast.LENGTH_SHORT).show();
+            super.handleMessage(msg);
+        }
+    };
+
+
+    // 线程服务类
+    private class AcceptThread extends Thread {
+        private BluetoothServerSocket serverSocket;
+        private BluetoothSocket socket;
+        // 输入 输出流
+        private OutputStream os;
+        private InputStream is;
+
+        public AcceptThread() {
+            try {
+                serverSocket = mBlueAdapter
+                        .listenUsingRfcommWithServiceRecord("同事", UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            // 截获客户端的蓝牙消息
+            try {
+                socket = serverSocket.accept(); // 如果阻塞了，就会一直停留在这里
+                is = socket.getInputStream();
+                os = socket.getOutputStream();
+                while (true) {
+                    synchronized (MainActivity.this) {
+                        byte[] tt = new byte[is.available()];
+                        if (tt.length > 0) {
+                            is.read(tt, 0, tt.length);
+                            Message msg = new Message();
+                            msg.obj = new String(tt, "GBK");
+                            handler.sendMessage(msg);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
